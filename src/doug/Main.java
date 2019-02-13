68ed73c1ffc97aa3d7781628ac95ddc80b05ace8
Douglas Hiura Longo <doulashiura@gmail.com>
2019-02-13 18:00:58 -0200

    https://github.com/bzon/PetClinicFitnesseSamples.git

--- src/doug/Main.java
+++ src/doug/Main.java
@@ -28,7 +28,7 @@ public class Main {
 		SuiteResponder suit = new SuiteResponder();
 		FitNesseContext context = ContextConfigurator.systemDefaults().makeFitNesseContext();
 		Request request = new Request(new ByteArrayInputStream("".getBytes()));
-		request.setResource(".CarRentalProject");
+		request.setResource(".petClinic");
 		suit.makeResponse(context, request);
 		List<WikiPage> pages = suit.getPagesToRun();
 		PagesByTestSystem pagesByTestSystem = new PagesByTestSystem(pages, context.getRootPage());
