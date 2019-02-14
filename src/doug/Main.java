8177fe66f96d60f98852789cb9167e09088ae105
Douglas Hiura Longo <doulashiura@gmail.com>
2019-02-14 18:51:18 -0200

    https://github.com/lsu-ub-uu/cora-fitnesse.git

--- src/doug/Main.java
+++ src/doug/Main.java
@@ -28,7 +28,7 @@ public class Main {
 		SuiteResponder suit = new SuiteResponder();
 		FitNesseContext context = ContextConfigurator.systemDefaults().makeFitNesseContext();
 		Request request = new Request(new ByteArrayInputStream("".getBytes()));
-		request.setResource(".BankApp");
+		request.setResource(".TheRestTestsCora");
 		suit.makeResponse(context, request);
 		List<WikiPage> pages = suit.getPagesToRun();
 		PagesByTestSystem pagesByTestSystem = new PagesByTestSystem(pages, context.getRootPage());
